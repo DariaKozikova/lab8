dfcb926ae525bcff0335caf1f5e871a11a98b37d
Daria bc2025-7 <kozikovadasa@gmail.com>
2026-04-18 17:31:01 +0300

    made the logo clickable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link, NavLink} from "react-router-dom"
 const Navbar = () => {
     return (
         <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
-            <div className="text-xl font-bold">Warehouse System</div>
+           <Link to="/" className="text-xl font-bold hover:text-gray-300 transition-colors">Warehouse System</Link>
             <div className="flex gap-4">
                 <NavLink to="/">Gallery</NavLink>
                 <NavLink to="/favorites">Favourites</NavLink>
